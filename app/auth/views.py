047f2fd3e4a6db606e062471676c0fe5f55fddc1
EugeneZnm <eugenenzioki@gmail.com>
2018-09-15 16:15:16 +0300

    login html

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,3 +6,48 @@ from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
 from ..email import mail_message
+
+
+# view function
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    login function to render template file
+    :return:
+    """
+    login_form=LoginForm() # instance of Login form passed into login template
+    if login_form.validate_on_submit():
+        """
+        checking form validation
+        """
+        user=User.query.filter_by(email = login_form.email.data).first()
+        """
+        searching for user in database with email received from form
+        """
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Pitches login"
+    return render_template('auth/login.html', login_form=login_form, title=title)
+
+
+@auth.route('/register', methods=['GET','POST'])
+def register():
+    """
+    function to render registration form
+    :return:
+    """
+    form = RegistrationForm()
+    if form.validate_on_submit():
+
+        user = User(username=form.username.data, email=form.email.data, age=form.age.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        title = "Welcome New User"
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', registration_form=form)
