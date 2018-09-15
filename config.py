0f17afc9601f0d67b3c210432b27d386b579f185
EugeneZnm <eugenenzioki@gmail.com>
2018-09-15 11:28:52 +0300

    parent configuration

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+
+class Config:
+    """
+    general configuration parent class
+
+    """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://eugene:necromancer@localhost/blog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CON = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
