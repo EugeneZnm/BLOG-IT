be8fc430fff2b115fc083372e8dd63f9f3b378fb
EugeneZnm <eugenenzioki@gmail.com>
2018-09-15 12:38:34 +0300

    instantiating bootatrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_simplemde import SimpleMDE
 
+# creation of instance of login
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     """
