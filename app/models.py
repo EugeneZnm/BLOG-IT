13013a1f04c3fd1f436f4971d7267292e3bf0886
EugeneZnm <eugenenzioki@gmail.com>
2018-09-15 12:13:37 +0300

    user olumns

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-  importing db
+# importing db
 from . import db
 # security model providing haching functionality
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,3 +10,17 @@ from flask_login import UserMixin
 from . import login_manager
 
 from datetime import datetime
+
+
+class Writer(UserMixin, db.Model):
+    """
+    creating class writer for creating blog writer and connecting it to database via db.Model
+
+    """
+    __tablename__ = 'writer'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    pass_secure = db.Column(db.String(255))
+
+
