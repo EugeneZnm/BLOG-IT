4e48f3fe5b1543bf96701624e89ba12898e3bdd6
EugeneZnm <eugenenzioki@gmail.com>
2018-09-15 20:59:33 +0300

    cultural posts

--- app/main/forms.py
+++ app/main/forms.py
@@ -18,6 +18,7 @@ class BlogForm(FlaskForm):
     """
     class to create blog post form
     """
-    post = TextAreaField('Pitch Goes Here')
-    category = RadioField('Categories', choices = [('lifestyle', 'lifestyle'),('Business', 'Business'),('Entertainment', 'Entertainment'), ('Motivational','Motivational')],validators=[Required()])
+    title = StringField('Blog post title')
+    post = TextAreaField('Blog Content')
+    category = RadioField('Categories', choices = [('lifestyle', 'lifestyle'),('Business', 'Business'),('Entertainment', 'Entertainment'), ('Culture','Culture')],validators=[Required()])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -40,6 +40,47 @@ def post():
     return render_template('new-pitch.html',post=posts)
 
 
+# Blog post categories
+@main.route('/Lifestyle', methods = ['GET', 'POST'])
+def promote():
+    """
+    displaying lifestyle blog post
+    """
+    lifestyle =Post.query.filter_by(category="Lifestyle").all()
+
+    return render_template('lifestyle.html', post =lifestyle)
+
+
+@main.route('/Business', methods = ['GET', 'POST'])
+def product():
+    """
+    show business posts
+    """
+    business = Post.query.filter_by(category="business").all()
+
+    return render_template('Business.html', post=business)
+
+
+@main.route('/entertainment', methods = ['GET', 'POST'])
+def entertainment():
+    """
+     show entertainment posts
+    """
+    entertainment = Post.query.filter_by(category="entertainment").all()
+
+    return render_template('Entertainment.html', post=entertainment)
+
+
+@main.route('/culture', methods = ['GET', 'POST'])
+def culture():
+    """
+    show culture posts
+    """
+    culture = Post.query.filter_by(category='culture').all()
+
+    return render_template('culture.html', post=culture)
+
+
 # display comments
 @main.route('/comments/<int:id>', methods = ['GET', 'POST'])
 def comments(id):
@@ -47,12 +88,12 @@ def comments(id):
     show comments
     """
     comment = CommentForm()
-    comment_is = Comments.query.filter_by(pitch_id=id)
+    comment_is = Comments.query.filter_by(post_id=id)
     if comment.validate_on_submit():
 
-        comments = Comments(saying=comment.saying.data,pitch_id=id, user_id=current_user.id )
+        comments = Comments(comment=comment.saying.data,post_id=id)
         comments.save_comments()
-        comment_is= Comments.query.filter_by(pitch_id=id)
+        comment_is= Comments.query.filter_by(post_id=id)
         return render_template('comments.html', comment=comment, comment_is=comment_is)
 
     return render_template('comments.html', comment=comment, comment_is=comment_is)
