ca5ed60bbc94f0a518ebf44593ac52f3b303330f
EugeneZnm <eugenenzioki@gmail.com>
2018-09-15 12:23:21 +0300

    setting password for writer

--- app/models.py
+++ app/models.py
@@ -23,4 +23,26 @@ class Writer(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     pass_secure = db.Column(db.String(255))
 
+    # call  back function retrieving writer id
+    @login_manager.writer_loader
+    def load_writer(writer_id):
+        return Writer.query.get(int(writer_id))
+
+    def set_password(self, password):
+        """
+        method to set passwords
+        """
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        method to verify password
+        """
+        return check_password_hash(self.pass_secure, password)
+
+    def  __repr__(self):
+        return f'Writer {self.username}'
+
+
+
 
