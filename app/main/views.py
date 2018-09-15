46f3ecfa7480290506b856cd4a3aa73a98e00f82
EugeneZnm <eugenenzioki@gmail.com>
2018-09-16 00:23:15 +0300

    comments styling

--- app/main/views.py
+++ app/main/views.py
@@ -33,11 +33,22 @@ def post():
 
     if posts.validate_on_submit():
 
-        posted = Post(post=posts.pitch.data, category=posts.category.data)
-        posted.save_pitch()
+        posted = Post(post=posts.post.data, category=posts.category.data, title=posts.title.data)
+        posted.save_post()
         return redirect(url_for('main.index'))
 
-    return render_template('new-pitch.html',post=posts)
+    # postit = Post.query.all()
+
+    return render_template('blog-post.html',posts=posts, postit=postit)
+
+
+# @main.route('/Post/<int:id>')
+# def single_post(category):
+#     pot = Post.query.get(category)
+#     if pot is None:
+#         abort(404)
+#     format_post = markdown2.markdown(pot.post, extras=["code-friendly", "fenced-code-blocks"])
+#     return render_template('singleblog.html', post = pot, format_post=format_post)
 
 
 # Blog post categories
@@ -58,7 +69,7 @@ def business():
     """
     business = Post.query.filter_by(category="business").all()
 
-    return render_template('Business.html', post=business)
+    return render_template('business.html', post=business)
 
 
 @main.route('/entertainment', methods = ['GET', 'POST'])
@@ -68,7 +79,7 @@ def entertainment():
     """
     entertainment = Post.query.filter_by(category="entertainment").all()
 
-    return render_template('Entertainment.html', post=entertainment)
+    return render_template('entertainment.html', post=entertainment)
 
 
 @main.route('/culture', methods = ['GET', 'POST'])
@@ -91,7 +102,7 @@ def comments(id):
     comment_is = Comments.query.filter_by(post_id=id)
     if comment.validate_on_submit():
 
-        comments = Comments(comment=comment.saying.data,post_id=id)
+        comments = Comments(comment=comment.comment.data,post_id=id)
         comments.save_comments()
         comment_is= Comments.query.filter_by(post_id=id)
         return render_template('comments.html', comment=comment, comment_is=comment_is)
