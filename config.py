a8a5d941b407930664f5f8f66136c4104cfc77bc
EugeneZnm <eugenenzioki@gmail.com>
2018-09-15 11:30:44 +0300

    prod config

--- config.py
+++ config.py
@@ -13,8 +13,23 @@ class Config:
 
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CON = True
+
+    # email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    """
+    production configuration child class
+    """
+
+class TestConfig(Config):
+    """
+    tests configuration class
+    """
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://eugene:necromancer@localhost/pitches_test'
+
