9138b9435b207de85da6251eff7db465bd0731bf
EugeneZnm <eugenenzioki@gmail.com>
2018-09-15 11:11:40 +0300

    manager shell decorator to create shell context

--- manage.py
+++ manage.py
@@ -10,3 +10,20 @@ manager =Manager(app)
 # command to launch application server
 manager.add_command('server', Server)
 
+@manager.command
+def test():
+    """
+    run tests
+    """
+    import unittest
+    tests =unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+# manager shell decorator to create shell context
+@manager.shell
+def make_shell_context():
+    """
+    shell context function allowing passing of properties into our shell
+    :return:
+    """
+    return dict(app=app, db=db, User=User,)
